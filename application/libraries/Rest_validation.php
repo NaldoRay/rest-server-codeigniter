e09b10ea7056443ac55630c935a7815f967c1ad2
Ray Naldo <raynaldo@unpar.ac.id>
2017-10-16 13:21:52 +0700

    Include missing 'FilesValidator' dependency

--- application/libraries/Rest_validation.php
+++ application/libraries/Rest_validation.php
@@ -1,6 +1,9 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+include_once('validation/ValueValidator.php');
 include_once('validation/ArrayValidator.php');
+include_once('validation/FilesValidator.php');
 
 /**
  * Author: RN
