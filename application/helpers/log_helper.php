e86b13b11c94b406036f6ce941d150c7025ff2bc
Ray Naldo <raynaldo@unpar.ac.id>
2017-12-22 10:37:05 +0700

    Support weekly transaction log

--- application/helpers/log_helper.php
+++ application/helpers/log_helper.php
@@ -2,60 +2,75 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
-    Author: Ray Naldo
+Author: Ray Naldo
  */
-defined('MONTHLY') OR define ('MONTHLY', 1);
-defined('YEARLY') OR define ('YEARLY', 2);
+defined('WEEKLY') OR define ('WEEKLY', 1);
+defined('MONTHLY') OR define ('MONTHLY', 2);
+defined('YEARLY') OR define ('YEARLY', 3);
 defined('LOG_PATH') OR define ('LOG_PATH', FCPATH.'logs/');
 
 if (!function_exists('logQuery'))
 {
-	/**
-	 * Log query ke file.
-	 *
-	 * @param string $query query to be logged
-	 * @param int $category to group logs; set MONTHLY (default), or YEARLY
-	 */
-	function logQuery ($query, $category = MONTHLY)
-	{
-		$CI =& get_instance();
+    /**
+     * Log query ke file.
+     *
+     * @param string $query query to be logged
+     * @param int $category to group logs; set MONTHLY (default), or YEARLY
+     */
+    function logQuery ($query, $category = MONTHLY)
+    {
+        $CI =& get_instance();
 
-		$appName = $CI->config->item('app_name');
-		if ($category === YEARLY)
-			$date = date("Y");
-		else
-			$date = date("Ym");
-		$fileName = $appName.'_'.$date.'.txt';
+        switch ($category)
+        {
+            case YEARLY:
+                $date = date("Y");
+                break;
+            case WEEKLY:
+                $date = sprintf('%s-week%s', date('Ym'), date('W'));
+                break;
+            default:
+                $date = date("Ym");
+                break;
+        }
+        $appName = $CI->config->item('app_name');
+        $fileName = $appName.'_'.$date.'.txt';
 
-		$ipAddress = $CI->input->ip_address();
-		$timestamp = date("Y-m-d H:i:s");
-		$logText = '['.$timestamp.'|'.$ipAddress.'|'.$query.']'.PHP_EOL;
+        $ipAddress = $CI->input->ip_address();
+        $timestamp = date("Y-m-d H:i:s");
+        $logText = '['.$timestamp.'|'.$ipAddress.'|'.$query.']'.PHP_EOL;
 
-		$filePath = LOG_PATH.$fileName;
-		write_file($filePath, $logText, 'a');
-	}
+        $filePath = LOG_PATH.$fileName;
+        write_file($filePath, $logText, 'a');
+    }
 }
 
 if (!function_exists('logFailedQuery'))
 {
-	function logFailedQuery ($query, $category = MONTHLY)
-	{
-		$CI =& get_instance();
+    function logFailedQuery ($query, $category = MONTHLY)
+    {
+        $CI =& get_instance();
 
-		$appName = $CI->config->item('app_name');
-		if ($category === YEARLY)
-			$date = date("Y");
-		else
-			$date = date("Ym");
-		
-		$fileName = $appName.'_'.$date.'_failed.txt';
-		$filePath = LOG_PATH.$fileName;
+        switch ($category)
+        {
+            case YEARLY:
+                $date = date("Y");
+                break;
+            case WEEKLY:
+                $date = sprintf('%s-week%s', date('Ym'), date('W'));
+                break;
+            default:
+                $date = date("Ym");
+                break;
+        }
+        $appName = $CI->config->item('app_name');
+        $fileName = $appName.'_'.$date.'_failed.txt';
+        $filePath = LOG_PATH.$fileName;
 
-		$ipAddress = $CI->input->ip_address();
-		$timestamp = date("Y-m-d H:i:s");
-		$logText = '['.$timestamp.'|'.$ipAddress.'|'.$query.']'.PHP_EOL;
-		
-		write_file($filePath, $logText, 'a');
-	}
+        $ipAddress = $CI->input->ip_address();
+        $timestamp = date("Y-m-d H:i:s");
+        $logText = '['.$timestamp.'|'.$ipAddress.'|'.$query.']'.PHP_EOL;
+
+        write_file($filePath, $logText, 'a');
+    }
 }
-
