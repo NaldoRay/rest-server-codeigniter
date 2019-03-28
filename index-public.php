d0da5e98145d51021cafbceb0921eca79e5021d0
Ray Naldo <raynaldo@unpar.ac.id>
2019-03-28 10:22:23 +0700

    Separate `index.php`, `.htaccess`, and build for bti and public release

--- index-public.php
+++ index-public.php
@@ -97,7 +97,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = '/project/systems/3.1.4';
+	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '/project/api/application';
+	$application_folder = 'application';
 
 /*
  *---------------------------------------------------------------
