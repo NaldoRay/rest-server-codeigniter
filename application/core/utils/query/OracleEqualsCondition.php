e52eae18e384db4cfa3d239beb17f9be2e2e1d9d
Ray Naldo <raynaldo@unpar.ac.id>
2019-04-22 15:27:52 +0700

    Fix regex error (in CI core) in `OracleEqualsCondition` and `OracleNotEqualsCondition` when multi-value comparison condition is too long

--- application/core/utils/query/OracleEqualsCondition.php
+++ application/core/utils/query/OracleEqualsCondition.php
@@ -13,20 +13,24 @@ class OracleEqualsCondition extends EqualsCondition
         $value = $this->getValue();
         if (is_array($value))
         {
-            if (count($value) > 1000)
+            $count = count($value);
+            if ($count > 1000)
             {
                 /*
-                 * Because Oracle implemented normal IN list with a limit of 1,000 elements,
-                 * then as a workaround, use multi-value comparison.
-                 * More than or equals to 100,000 elements will not handled as currently there's no workaround.
-                 * https://stackoverflow.com/a/17019130
+                 * Because Oracle implemented normal IN (...) with a limit of 1,000 elements,
+                 * As a workaround, there's multi-value comparison. More than or equals to 100,000 elements will not handled as currently there's no workaround.
+                 * Reference: https://stackoverflow.com/a/17019130
+                 * But CodeIgniter is not able to parse the regex if the value is too much.
+                 * After experimenting, although slow, the most robust way is by using multiple IN (...) joined with OR.
                  */
+                $field = $this->getField();
+                $whereInArr = array();
+                for ($i = 0; $i < $count; $i += 1000)
+                {
+                    $whereInArr[] = sprintf("%s IN (%s)", $field, implode(',', array_slice($value, $i, 1000)));
+                }
 
-                $multiValues = array();
-                foreach ($value as $val)
-                    $multiValues[] = sprintf("(1,%s)", $val);
-
-                return sprintf("(1,%s) IN (%s)", $this->getField(), implode(',', $multiValues));
+                return sprintf("(%s)", implode(' OR ', $whereInArr));
             }
         }
 
